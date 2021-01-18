038407721191e6f0c733108a32d58d8fc9352ae7
Zumwani <andreas.ingeholm@gmail.com>
2021-01-18 17:21:42 +0100

    Added Common.VersionIncrementer, which can increment <Version> tag in csproj, when used in a pre build event.

---
+++ Common.VersionIncrementer/App.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Common.VersionIncrementer
+{
+
+    static class App
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                var path = GetProjectFile(args);
+                var text = File.ReadAllText(path);
+                Increment(ref text);
+
+                File.WriteAllText(path, text);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetType().Name + ":");
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+
+        }
+
+        static string GetProjectFile(string[] args)
+        {
+
+            var projectFile = args.FirstOrDefault();
+
+            if (!File.Exists(projectFile))
+                throw new FileNotFoundException(message: null, fileName: projectFile);
+
+            if (!projectFile.EndsWith(".csproj"))
+                throw new ArgumentException("Invalid project file");
+
+            return projectFile;
+
+        }
+
+        static void Increment(ref string text)
+        {
+
+            if (!(text.Contains("<Version>") && text.Contains("</Version>")))
+                throw new ArgumentException("No version tag exists.");
+
+            var match = Regex.Match(text, "<Version>(.*)</Version>");
+            var currentVersion = new Version(match.Groups[1].ToString());
+            var newVersion = new Version(currentVersion.Major, currentVersion.Minor, currentVersion.Build + 1);
+
+            text = text.Replace(match.Value, $"<Version>{newVersion}</Version>");
+
+        }
+
+    }
+
+}
