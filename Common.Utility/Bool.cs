90da11b7c70a051a8af9c5f53893e77073105595
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-07-21 18:55:18 +0200

    Changed {x:True} to public.

--- Common.Utility/Bool.cs
+++ Common.Utility/Bool.cs
@@ -4,12 +4,12 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml", "Common.Utility.Bool")]
 namespace Common.Utility.Bool;
 
-class TrueExtension : MarkupExtension
+public class TrueExtension : MarkupExtension
 {
     public override object ProvideValue(IServiceProvider serviceProvider) => true;
 }
 
-class FalseExtension : MarkupExtension
+public class FalseExtension : MarkupExtension
 {
     public override object ProvideValue(IServiceProvider serviceProvider) => false;
 }
