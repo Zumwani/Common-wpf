2aa74660ab43a7734f17a6827b8af918c1190603
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-07-21 18:41:19 +0200

    Added {x:True} which for some reason does not exist in wpf already.

---
+++ Common.Utility/Bool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml", "Common.Utility.Bool")]
+namespace Common.Utility.Bool;
+
+class TrueExtension : MarkupExtension
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => true;
+}
+
+class FalseExtension : MarkupExtension
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => false;
+}
