f92e3c3c1298ac9477ed1a6079de1fa645a85f0c
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-07-26 08:15:24 +0200

    Create LinqUtility.cs
    - Added IEnumerable<T>.Except(this IEnumerable<T> list, T item).

---
+++ Common.Utility/Utility/LinqUtility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Utility;
+
+public static class LinqUtility
+{
+
+    /// <inheritdoc cref="Enumerable.Except"/>
+    public static IEnumerable<T> Except<T>(this IEnumerable<T> list, T item) =>
+        list.Except(new[] { item });
+
+}
