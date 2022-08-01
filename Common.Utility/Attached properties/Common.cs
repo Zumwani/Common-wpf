aa75ef30bb4c1832bd87da9578bcabb5420a2606
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-08-01 15:38:43 +0200

    Moved transitions into Transitions rather than common.
    - Added default transitions.

--- Common.Utility/Attached properties/Common.cs
+++ Common.Utility/Attached properties/Common.cs
@@ -217,12 +217,12 @@ public static class Common
 
         if (e.NewValue is true)
         {
-            await DoTransition(GetShowTransition(element), element as FrameworkElement);
             element.Visibility = Visibility.Visible;
+            await Transition.DoTransition(Transition.GetShow(element), element as FrameworkElement);
         }
         else if (e.NewValue is false)
         {
-            await DoTransition(GetHideTransition(element), element as FrameworkElement);
+            await Transition.DoTransition(Transition.GetHide(element), element as FrameworkElement);
             element.Visibility = Visibility.Collapsed;
         }
 
@@ -245,13 +245,13 @@ public static class Common
 
         if (e.NewValue is true)
         {
-            await DoTransition(GetHideTransition(element), element as FrameworkElement);
+            await Transition.DoTransition(Transition.GetHide(element), element as FrameworkElement);
             element.Visibility = Visibility.Collapsed;
         }
         else if (e.NewValue is false)
         {
-            await DoTransition(GetShowTransition(element), element as FrameworkElement);
             element.Visibility = Visibility.Visible;
+            await Transition.DoTransition(Transition.GetShow(element), element as FrameworkElement);
         }
 
     }
@@ -273,53 +273,16 @@ public static class Common
 
         if (e.NewValue is true)
         {
-            await DoTransition(GetHideTransition(element), element as FrameworkElement);
+            await Transition.DoTransition(Transition.GetHide(element), element as FrameworkElement);
             element.Visibility = Visibility.Hidden;
         }
         else if (e.NewValue is false)
         {
-            await DoTransition(GetShowTransition(element), element as FrameworkElement);
             element.Visibility = Visibility.Visible;
+            await Transition.DoTransition(Transition.GetShow(element), element as FrameworkElement);
         }
 
     }
-
-    #endregion
-    #region Transitions
-
-    static Task DoTransition(Storyboard? storyboard, FrameworkElement? element)
-    {
-
-        if (storyboard is null || element is null)
-            return Task.CompletedTask;
-
-        var tcs = new TaskCompletionSource<bool>();
-        storyboard.Completed += Storyboard_Completed;
-        storyboard.RemoveRequested += Storyboard_Completed;
-        storyboard.Begin(element);
-
-        return tcs.Task;
-
-        void Storyboard_Completed(object? sender, EventArgs e)
-        {
-            storyboard.Completed -= Storyboard_Completed;
-            storyboard.RemoveRequested -= Storyboard_Completed;
-            tcs.SetResult(true);
-        }
-
-    }
-
-    public static Storyboard? GetShowTransition(UIElement obj) => (Storyboard?)obj.GetValue(ShowTransitionProperty);
-    public static void SetShowTransition(UIElement obj, Storyboard? value) => obj.SetValue(ShowTransitionProperty, value);
-
-    public static readonly DependencyProperty ShowTransitionProperty =
-        DependencyProperty.RegisterAttached("ShowTransition", typeof(Storyboard), typeof(Common), new PropertyMetadata(null));
-
-    public static Storyboard? GetHideTransition(UIElement obj) => (Storyboard?)obj.GetValue(HideTransitionProperty);
-    public static void SetHideTransition(UIElement obj, Storyboard? value) => obj.SetValue(HideTransitionProperty, value);
-
-    public static readonly DependencyProperty HideTransitionProperty =
-        DependencyProperty.RegisterAttached("HideTransition", typeof(Storyboard), typeof(Common), new PropertyMetadata(null));
 
     #endregion
 
@@ -1000,3 +963,78 @@ public static class Common
 
 }
 
+public static class Transition
+{
+
+    public static Task DoTransition(Storyboard? storyboard, FrameworkElement? element)
+    {
+
+        if (storyboard is null || element is null)
+            return Task.CompletedTask;
+
+        var tcs = new TaskCompletionSource<bool>();
+        storyboard.Completed += Storyboard_Completed;
+        storyboard.RemoveRequested += Storyboard_Completed;
+
+        storyboard.Begin(element);
+
+        return tcs.Task;
+
+        void Storyboard_Completed(object? sender, EventArgs e)
+        {
+            storyboard.Completed -= Storyboard_Completed;
+            storyboard.RemoveRequested -= Storyboard_Completed;
+            tcs.SetResult(true);
+        }
+
+    }
+
+    static readonly CubicEase ease = new();
+
+    #region Show
+
+    public static Storyboard GetShowDefault(double durationInSeconds = 0.25)
+    {
+        var storyboard = new Storyboard();
+        var animation = new DoubleAnimation { Duration = new(TimeSpan.FromSeconds(durationInSeconds)), To = 1, EasingFunction = ease };
+        Storyboard.SetTargetProperty(animation, new(nameof(UIElement.Opacity)));
+        storyboard.Children.Add(animation);
+        return storyboard;
+    }
+
+    public static bool GetUseDefaultShow(UIElement obj) => (bool)obj.GetValue(UseDefaultShowProperty);
+    public static void SetUseDefaultShow(UIElement obj, Storyboard? value) => obj.SetValue(UseDefaultShowProperty, value);
+
+    public static readonly DependencyProperty UseDefaultShowProperty = DependencyProperty.RegisterAttached("UseDefaultShow", typeof(bool), typeof(Common), new PropertyMetadata(true));
+
+    public static Storyboard? GetShow(UIElement obj) => (Storyboard?)obj.GetValue(ShowProperty) ?? (GetUseDefaultShow(obj) ? GetShowDefault() : null);
+    public static void SetShow(UIElement obj, Storyboard? value) => obj.SetValue(ShowProperty, value);
+
+    public static readonly DependencyProperty ShowProperty = DependencyProperty.RegisterAttached("Show", typeof(Storyboard), typeof(Common), new PropertyMetadata(null));
+
+    #endregion
+    #region Hide
+
+    public static Storyboard GetHideDefault(double durationInSeconds = 0.25)
+    {
+        var storyboard = new Storyboard();
+        var animation = new DoubleAnimation { Duration = new(TimeSpan.FromSeconds(durationInSeconds)), To = 0, EasingFunction = ease };
+        Storyboard.SetTargetProperty(animation, new(nameof(UIElement.Opacity)));
+        storyboard.Children.Add(animation);
+        return storyboard;
+    }
+
+    public static bool GetUseDefaultHide(UIElement obj) => (bool)obj.GetValue(UseDefaultHideProperty);
+    public static void SetUseDefaultHide(UIElement obj, bool value) => obj.SetValue(UseDefaultHideProperty, value);
+
+    public static readonly DependencyProperty UseDefaultHideProperty = DependencyProperty.RegisterAttached("UseDefaultHide", typeof(bool), typeof(Common), new PropertyMetadata(true));
+
+    public static Storyboard? GetHide(UIElement obj) => (Storyboard?)obj.GetValue(HideProperty) ?? (GetUseDefaultShow(obj) ? GetHideDefault() : null);
+    public static void SetHide(UIElement obj, Storyboard? value) => obj.SetValue(HideProperty, value);
+
+    public static readonly DependencyProperty HideProperty =
+        DependencyProperty.RegisterAttached("Hide", typeof(Storyboard), typeof(Common), new PropertyMetadata(null));
+
+    #endregion
+
+}
