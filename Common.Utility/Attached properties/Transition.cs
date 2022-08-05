b056d3eae3f9f36151395f7ff4d5c0749dc01f70
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-08-05 12:09:19 +0200

    Updated Transition

--- Common.Utility/Attached properties/Transition.cs
+++ Common.Utility/Attached properties/Transition.cs
@@ -8,16 +8,21 @@ namespace Common.Utility.AttachedProperties;
 public static class Transition
 {
 
-    public static Task DoTransition(Storyboard? storyboard, FrameworkElement? element)
+    public static async Task DoTransition(Storyboard? storyboard, FrameworkElement? element)
+    {
+        if (storyboard is not null || element is not null)
+            await storyboard.BeginAsync(element);
+    }
+
+    public static Task BeginAsync(this Storyboard? storyboard, FrameworkElement? element = null)
     {
 
-        if (storyboard is null || element is null)
+        if (storyboard is null || element is not null)
             return Task.CompletedTask;
 
         var tcs = new TaskCompletionSource<bool>();
         storyboard.Completed += Storyboard_Completed;
         storyboard.RemoveRequested += Storyboard_Completed;
-
         storyboard.Begin(element);
 
         return tcs.Task;
@@ -33,16 +38,19 @@ public static class Transition
 
     static readonly CubicEase ease = new();
 
-    #region Show
-
-    public static Storyboard GetShowDefault(double durationInSeconds = 0.25)
+    public static Storyboard FadeAnimation(double to, double durationInSeconds = 0.25)
     {
         var storyboard = new Storyboard();
-        var animation = new DoubleAnimation { Duration = new(TimeSpan.FromSeconds(durationInSeconds)), To = 1, EasingFunction = ease };
+        var animation = new DoubleAnimation { Duration = new(TimeSpan.FromSeconds(durationInSeconds)), To = to, EasingFunction = ease };
         Storyboard.SetTargetProperty(animation, new(nameof(UIElement.Opacity)));
         storyboard.Children.Add(animation);
         return storyboard;
     }
+
+    #region Show
+
+    public static Storyboard GetShowDefault(double durationInSeconds = 0.25) =>
+        FadeAnimation(1, durationInSeconds);
 
     public static bool GetUseDefaultShow(UIElement obj) => (bool)obj.GetValue(UseDefaultShowProperty);
     public static void SetUseDefaultShow(UIElement obj, bool value) => obj.SetValue(UseDefaultShowProperty, value);
@@ -57,14 +65,8 @@ public static class Transition
     #endregion
     #region Hide
 
-    public static Storyboard GetHideDefault(double durationInSeconds = 0.25)
-    {
-        var storyboard = new Storyboard();
-        var animation = new DoubleAnimation { Duration = new(TimeSpan.FromSeconds(durationInSeconds)), To = 0, EasingFunction = ease };
-        Storyboard.SetTargetProperty(animation, new(nameof(UIElement.Opacity)));
-        storyboard.Children.Add(animation);
-        return storyboard;
-    }
+    public static Storyboard GetHideDefault(double durationInSeconds = 0.25) =>
+        FadeAnimation(0, durationInSeconds);
 
     public static bool GetUseDefaultHide(UIElement obj) => (bool)obj.GetValue(UseDefaultHideProperty);
     public static void SetUseDefaultHide(UIElement obj, bool value) => obj.SetValue(UseDefaultHideProperty, value);
